beadb74b0e14cbd2489549980f34e8874bc9fdf7
Peter Ivanovskij <65976810+Ivanovskij-Peter@users.noreply.github.com>
2020-10-01 00:02:31 +0300

    добавил разметку домашней страницы

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React, { Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
+import { HomePage } from "../pages/homePage/HomePage";
 import { PageNotFound } from "../pages/pageNotFound/PageNotFound";
 
 export function App() {
   return (
     <>
-      {/* <Suspense fallback={<Spinner />}> */}
-      {/* <Switch> */}
-      {/* {routes.map(({ path, name, exact, component }) => {
+      {/* <HomePage /> */}
+      {/* <Suspense fallback={<Spinner />}>
+        <Switch>
+          {routes.map(({ path, name, exact, component }) => {
             return (
               <Route
                 key={name}
@@ -16,14 +18,14 @@ export function App() {
                 component={component}
               />
             );
-          })} */}
-      {/* <Route>
-        <PageNotFound>
-          <h1>PageNotFound</h1>
-        </PageNotFound>
-      </Route> */}
-      {/* </Switch> */}
-      {/* </Suspense> */}
+          })}
+          <Route>
+            <PageNotFound>
+              <h1>PageNotFound</h1>
+            </PageNotFound>
+          </Route>
+        </Switch>
+      </Suspense> */}
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-import {App} from './components/App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./components/App";
+import { HomePage } from "./pages/homePage/HomePage";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
+    <HomePage />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
