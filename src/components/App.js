ed24bd3f613859d91ae4de0ce3709c3af324e4e9
andrew65215584 <63922036+andrew65215584@users.noreply.github.com>
2020-09-30 21:19:45 +0300

    added route

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,78 @@
 import React, { Suspense } from "react";
-import { Switch, Route } from "react-router-dom";
-import { PageNotFound } from "../pages/pageNotFound/PageNotFound";
+import { Switch, Route, Redirect } from "react-router-dom";
+import PrivateRoute  from "./privateRoute/PrivateRoute";
+import  PublicRoute  from "./publicRoute/PublicRoute";
+// import {
+//   SubscriptionsPage,
+//   PageNotFound,
+//   ChooseAvatarPage,
+//   NotificationsPage,
+//   AchievementsPage,
+//   ProfilePage,
+//   DailyResultModal,
+//   HabitTemplateModal,
+//   HabitChoiceModal,
+//   CustomHabitModal,
+//   CongratulationModal,
+//   InterviewModal,
+//   CheckListPage,
+//   SignUp,
+//   LogIn,
+//   HomePage,
+// } from "../routes";
 
 export function App() {
   return (
     <>
-      {/* <Suspense fallback={<Spinner />}> */}
-      {/* <Switch> */}
-      {/* {routes.map(({ path, name, exact, component }) => {
-            return (
-              <Route
-                key={name}
-                path={path}
-                exact={exact}
-                component={component}
-              />
-            );
-          })} */}
-      {/* <Route>
-        <PageNotFound>
-          <h1>PageNotFound</h1>
-        </PageNotFound>
-      </Route> */}
-      {/* </Switch> */}
-      {/* </Suspense> */}
+      <Suspense fallback={<h2>Загружаем...</h2>}>
+        <Switch>
+          <PublicRoute exact path="/" /* component={HomePage} */ />
+          <PublicRoute path="/logIn" /*  component={LogIn}  */ />
+          <PublicRoute path="/signUp" /*  component={SignUp}  */ />
+
+          <PrivateRoute
+            path="/checkListPage/" /*  component={CheckListPage}  */
+          />
+
+          <PrivateRoute
+            path="/checkListPage/interviewModal" /*  component={InterviewModal}  */
+          />
+          <PrivateRoute
+            path="/checkListPage/congratulationModal" /*  component={CongratulationModal}  */
+          />
+          <PrivateRoute
+            path="/checkListPage/customHabitModal" /*  component={CustomHabitModal}  */
+          />
+          <PrivateRoute
+            path="/checkListPage/habitChoiceModal" /*  component={HabitChoiceModal}  */
+          />
+          <PrivateRoute
+            path="/checkListPage/habitTemplateModal" /*  component={HabitTemplateModal}  */
+          />
+          <PrivateRoute
+            path="/checkListPage/dailyResultModal" /*  component={DailyResultModal}  */
+          />
+
+          <PrivateRoute path="/profilePage" /*  component={ProfilePage}  */ />
+          <PrivateRoute
+            path="/achievementsPage" /*  component={AchievementsPage}  */
+          />
+          <PrivateRoute
+            path="/notificationsPage" /*  component={NotificationsPage}  */
+          />
+          <PrivateRoute
+            path="/subscriptionsPage" /*  component={SubscriptionsPage}  */
+          />
+          <PrivateRoute
+            path="/chooseAvatarPage" /*  component={ChooseAvatarPage}  */
+          />
+          <PrivateRoute path="/pageNotFound" /*  component={PageNotFound}  */ />
+
+         
+
+         
+        </Switch>
+      </Suspense>
     </>
   );
 }
---
+++ src/routes.js
@@ -0,0 +1,95 @@
+import { lazy } from "react";
+
+// const HomePage = lazy(() =>
+//   import("./components/HomePage/HomePage" /* webpackChunkName: "HomePage" */)
+// );
+// const LogIn = lazy(() =>
+//   import("./components/logIn/logIn" /* webpackChunkName: "logIn" */)
+// );
+// const SignUp = lazy(() =>
+//   import("./components/signUp/signUp" /* webpackChunkName: "signUp" */)
+// );
+// const CheckListPage = lazy(() =>
+//   import(
+//     "./components/checkListPage/checkListPage" /* webpackChunkName: "checkListPage" */
+//   )
+// );
+// const InterviewModal = lazy(() =>
+//   import(
+//     "./components/interviewModal/interviewModal" /* webpackChunkName: "interviewModal" */
+//   )
+// );
+// const CongratulationModal = lazy(() =>
+//   import(
+//     "./components/congratulationModal/congratulationModal" /* webpackChunkName: "congratulationModal" */
+//   )
+// );
+// const CustomHabitModal = lazy(() =>
+//   import(
+//     "./components/customHabitModal/customHabitModal" /* webpackChunkName: "customHabitModal" */
+//   )
+// );
+// const HabitChoiceModal = lazy(() =>
+//   import(
+//     "./components/habitChoiceModal/habitChoiceModal" /* webpackChunkName: "habitChoiceModal" */
+//   )
+// );
+// const HabitTemplateModal = lazy(() =>
+//   import(
+//     "./components/habitTemplateModal/habitTemplateModal" /* webpackChunkName: "habitTemplateModal" */
+//   )
+// );
+// const DailyResultModal = lazy(() =>
+//   import(
+//     "./components/dailyResultModal/dailyResultModal" /* webpackChunkName: "dailyResultModal" */
+//   )
+// );
+// const ProfilePage = lazy(() =>
+//   import(
+//     "./components/profilePage/profilePage" /* webpackChunkName: "profilePage" */
+//   )
+// );
+// const AchievementsPage = lazy(() =>
+//   import(
+//     "./components/achievementsPage/achievementsPage" /* webpackChunkName: "achievementsPage" */
+//   )
+// );
+// const NotificationsPage = lazy(() =>
+//   import(
+//     "./components/notificationsPage/notificationsPage" /* webpackChunkName: "notificationsPage" */
+//   )
+// );
+// const ChooseAvatarPage = lazy(() =>
+//   import(
+//     "./components/chooseAvatarPage/chooseAvatarPage" /* webpackChunkName: "chooseAvatarPage" */
+//   )
+// );
+// const SubscriptionsPage = lazy(() =>
+//   import(
+//     "./components/subscriptionsPage/subscriptionsPage" /* webpackChunkName: "subscriptionsPage" */
+//   )
+// );
+// const PageNotFound = lazy(() =>
+//   import(
+//     "./components/pageNotFound/pageNotFound" /* webpackChunkName: "pageNotFound" */
+//   )
+// );
+
+// export default {
+//   SubscriptionsPage,
+//   PageNotFound,
+//   ChooseAvatarPage,
+//   NotificationsPage,
+//   AchievementsPage,
+//   ProfilePage,
+//   DailyResultModal,
+//   HabitTemplateModal,
+//   HabitChoiceModal,
+//   CustomHabitModal,
+//   CongratulationModal,
+//   InterviewModal,
+//   CheckListPage,
+//   SignUp,
+//   LogIn,
+//   HomePage,
+// };
