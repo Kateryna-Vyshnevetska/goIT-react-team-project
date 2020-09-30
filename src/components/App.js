40b3420b828e11ab7eced99eeac3966001ce52c3
Kateryna-Kopchuk <vyshnevetska.k@gmail.com>
2020-10-01 00:35:52 +0300

    fixed day 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Suspense } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { HomePage } from "../pages/homePage/HomePage";
-import PrivateRoute  from "./privateRoute/PrivateRoute";
-import  PublicRoute  from "./publicRoute/PublicRoute";
+import PrivateRoute from "./privateRoute/PrivateRoute";
+import PublicRoute from "./publicRoute/PublicRoute";
 import { PageNotFound } from "../pages/pageNotFound/PageNotFound";
+import { BasicInput } from "./BasicInput/BasicInput";
+import CustomButton from "./CustomButton/CustomButton";
 // import {
 //   SubscriptionsPage,
 //   PageNotFound,
@@ -17,36 +19,38 @@ import { PageNotFound } from "../pages/pageNotFound/PageNotFound";
 //   HomePage,
 // } from "../routes";
 
-
 export function App() {
   return (
     <>
-      <Suspense fallback={<h2>Загружаем...</h2>}>
-        <Switch>
-          <PublicRoute exact path="/" /* component={HomePage} */ />
-          <PublicRoute path="/logIn" /*  component={LogIn}  */ />
-          <PublicRoute path="/signUp" /*  component={SignUp}  */ />
+      {/* <Suspense fallback={<h2>Загружаем...</h2>}> */}
+      {/* <Switch> */}
+      {/* <PublicRoute exact path="/" /* component={HomePage} */}
+      {/* <PublicRoute path="/logIn" /*  component={LogIn}  */
+      /*/}
+          {/* <PublicRoute path="/signUp" /*  component={SignUp}  */
+      /*/}
 
-          <PrivateRoute
-            path="/checkListPage/" /*  component={CheckListPage}  */
-          />
-          
-          <PrivateRoute path="/profilePage" /*  component={ProfilePage}  */ />
-          <PrivateRoute
-            path="/achievementsPage" /*  component={AchievementsPage}  */
-          />
-          <PrivateRoute
-            path="/notificationsPage" /*  component={NotificationsPage}  */
-          />
-          <PrivateRoute
-            path="/subscriptionsPage" /*  component={SubscriptionsPage}  */
-          />
-          <PrivateRoute
-            path="/chooseAvatarPage" /*  component={ChooseAvatarPage}  */
-          />
-          <PrivateRoute path="/pageNotFound" /*  component={PageNotFound}  */ />
-        </Switch>
-      </Suspense>
+          {/* <PrivateRoute */}
+      {/* path="/checkListPage/" /*  component={CheckListPage}  */}
+      {/* /> */}
+
+      {/* <PrivateRoute path="/profilePage" /*  component={ProfilePage}  */
+      /*/}
+          {/* <PrivateRoute */}
+      {/* path="/achievementsPage" /*  component={AchievementsPage}  */}
+      {/* /> */}
+      {/* <PrivateRoute */}
+      {/* path="/notificationsPage" /*  component={NotificationsPage}  */}
+      {/* /> */}
+      {/* <PrivateRoute */}
+      {/* path="/subscriptionsPage" /*  component={SubscriptionsPage}  */}
+      {/* /> */}
+      {/* <PrivateRoute */}
+      {/* path="/chooseAvatarPage" /*  component={ChooseAvatarPage}  */}
+      {/* /> */}
+      {/* <PrivateRoute path="/pageNotFound" /*  component={PageNotFound}  */}
+      {/* </Switch> */}
+      {/* </Suspense> */}
       {/* <HomePage /> */}
     </>
   );
