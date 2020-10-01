df723bdc5b913d6143637e68c818c70e4201fbfb
Tofik <smilokiller@mail.ru>
2020-10-01 09:33:44 +0300

    create Hoc

--- src/components/modalBackDrop/ModalBackDrop.js
+++ src/components/modalBackDrop/ModalBackDrop.js
@@ -1,11 +1,54 @@
 import React from "react";
+import { CSSTransition } from "react-transition-group";
 
 const ModalBackDrop = (WrappedComponent) => {
-  class HOC extends React.Component {
-    render() {
-      return <WrappedComponent />;
-    }
-  }
+  return class HOC extends React.Component {
+    state = {
+      isOpen: false,
+    };
 
-  return HOC;
+    componentDidMount() {
+      this.setState({ isOpen: true });
+      window.addEventListener("keydown", this.closeModalKeydown);
+      document.addEventListener("click", this.closeModalOverlay);
+      document.styleSheets.overflow = "hidden";
+    }
+
+    componentWillUnmount() {
+      window.removeEventListener("keydown", this.closeModalKeydown);
+      document.removeEventListener("click", this.closeModalOverlay);
+      document.styleSheets.overflow = "auto";
+    }
+
+    closeModal = () => {
+      this.props.close();
+    };
+
+    closeModalKeydown = (evt) => {
+      if (evt.code === "Escape") {
+        this.closeModal();
+      }
+    };
+
+    closeModalOverlay = (evt) => {
+      if (evt.target.dataset.type === "modal") {
+        this.closeModal();
+      }
+    };
+
+    render() {
+      return (
+        <div data-type="modal" classname={}>
+          <CSSTransition
+            in={this.state.isOpen}
+            timeout={250}
+            classname={}
+            unmountOnExit
+          >
+            <WrappedComponent {...this.props} classname={} />
+          </CSSTransition>
+        </div>
+      );
+    }
+  };
 };
