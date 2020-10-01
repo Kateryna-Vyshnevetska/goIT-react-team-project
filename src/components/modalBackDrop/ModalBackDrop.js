dc4cde7a0adbaa3ac1960e750521c5b6fcadf5a6
Kateryna-Kopchuk <vyshnevetska.k@gmail.com>
2020-10-01 09:44:53 +0300

    fixed checkList

--- src/components/modalBackDrop/ModalBackDrop.js
+++ src/components/modalBackDrop/ModalBackDrop.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { CSSTransition } from "react-transition-group";
 
-const ModalBackDrop = (WrappedComponent) => {
+const modalBackDrop = (WrappedComponent) => {
   return class HOC extends React.Component {
-    
     state = {
       isOpen: false,
     };
