303b73f6163a5d7a475d18cb518d34b86eee0633
grebenuks <grebenuks2406@gmail.com>
2020-10-01 17:08:58 +0300

    progressBar fixed

--- src/pages/checkListPage/HabitItem.js
+++ src/pages/checkListPage/HabitItem.js
@@ -13,33 +13,119 @@ export function HabitItem({
   linearProgressValue = 10,
 }) {
   return (
-    <li className="habit-item" style={{ borderColor: getRandomColor() }}>
-      <div className="habit-scale-container">
-        <h3 className="habit-title">{habitTitle}</h3>
-        <div className="habit-scale">
-          <LinearProgressWithLabel value={linearProgressValue} />
-          <button
-            className="btn done"
-            onClick={handleClickHabitButtonDone}
-          ></button>
-          <button
-            className="btn missed"
-            onClick={handleClickHabitButtonMissed}
-          ></button>
+    <>
+      <li className="habit-item" style={{ borderColor: getRandomColor() }}>
+        <div className="habit-scale-container">
+          <h3 className="habit-title">{habitTitle}</h3>
+          <div className="habit-scale">
+            <LinearProgressWithLabel value={linearProgressValue} />
+            <button
+              className="btn done"
+              onClick={handleClickHabitButtonDone}
+            ></button>
+            <button
+              className="btn missed"
+              onClick={handleClickHabitButtonMissed}
+            ></button>
+          </div>
+          <p className="habit-scale-text">Прогресс привития привычки</p>
+          <button className="btn-settings"></button>
         </div>
-        <p className="habit-scale-text">Прогресс привития привычки</p>
-        <button className="btn-settings"></button>
-      </div>
-      <div className="habit-number-counter">
-        <div className="habit-done">
-          <p>К-во выполненных дней</p>
-          <span className="habit-done-number">{habitDoneNumber}</span>
+        <div className="habit-number-counter">
+          <div className="habit-done">
+            <p>К-во выполненных дней</p>
+            <span className="habit-done-number">{habitDoneNumber}</span>
+          </div>
+          <div className="habit-missed">
+            <p>К-во пропущенных дней</p>
+            <span className="habit-missed-number">{habitMissedNumber}</span>
+          </div>
         </div>
-        <div className="habit-missed">
-          <p>К-во пропущенных дней</p>
-          <span className="habit-missed-number">{habitMissedNumber}</span>
+      </li>
+      <li className="habit-item" style={{ borderColor: getRandomColor() }}>
+        <div className="habit-scale-container">
+          <h3 className="habit-title">{habitTitle}</h3>
+          <div className="habit-scale">
+            <LinearProgressWithLabel value={linearProgressValue} />
+            <button
+              className="btn done"
+              onClick={handleClickHabitButtonDone}
+            ></button>
+            <button
+              className="btn missed"
+              onClick={handleClickHabitButtonMissed}
+            ></button>
+          </div>
+          <p className="habit-scale-text">Прогресс привития привычки</p>
+          <button className="btn-settings"></button>
         </div>
-      </div>
-    </li>
+        <div className="habit-number-counter">
+          <div className="habit-done">
+            <p>К-во выполненных дней</p>
+            <span className="habit-done-number">{habitDoneNumber}</span>
+          </div>
+          <div className="habit-missed">
+            <p>К-во пропущенных дней</p>
+            <span className="habit-missed-number">{habitMissedNumber}</span>
+          </div>
+        </div>
+      </li>
+      <li className="habit-item" style={{ borderColor: getRandomColor() }}>
+        <div className="habit-scale-container">
+          <h3 className="habit-title">{habitTitle}</h3>
+          <div className="habit-scale">
+            <LinearProgressWithLabel value={linearProgressValue} />
+            <button
+              className="btn done"
+              onClick={handleClickHabitButtonDone}
+            ></button>
+            <button
+              className="btn missed"
+              onClick={handleClickHabitButtonMissed}
+            ></button>
+          </div>
+          <p className="habit-scale-text">Прогресс привития привычки</p>
+          <button className="btn-settings"></button>
+        </div>
+        <div className="habit-number-counter">
+          <div className="habit-done">
+            <p>К-во выполненных дней</p>
+            <span className="habit-done-number">{habitDoneNumber}</span>
+          </div>
+          <div className="habit-missed">
+            <p>К-во пропущенных дней</p>
+            <span className="habit-missed-number">{habitMissedNumber}</span>
+          </div>
+        </div>
+      </li>
+      <li className="habit-item" style={{ borderColor: getRandomColor() }}>
+        <div className="habit-scale-container">
+          <h3 className="habit-title">{habitTitle}</h3>
+          <div className="habit-scale">
+            <LinearProgressWithLabel value={linearProgressValue} />
+            <button
+              className="btn done"
+              onClick={handleClickHabitButtonDone}
+            ></button>
+            <button
+              className="btn missed"
+              onClick={handleClickHabitButtonMissed}
+            ></button>
+          </div>
+          <p className="habit-scale-text">Прогресс привития привычки</p>
+          <button className="btn-settings"></button>
+        </div>
+        <div className="habit-number-counter">
+          <div className="habit-done">
+            <p>К-во выполненных дней</p>
+            <span className="habit-done-number">{habitDoneNumber}</span>
+          </div>
+          <div className="habit-missed">
+            <p>К-во пропущенных дней</p>
+            <span className="habit-missed-number">{habitMissedNumber}</span>
+          </div>
+        </div>
+      </li>
+    </>
   );
 }
