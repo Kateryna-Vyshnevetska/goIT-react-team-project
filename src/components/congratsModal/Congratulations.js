bda7616911aebb623d7315b115865302bf4da3be
Oleksandr Pushkar <64772614+tauruskin@users.noreply.github.com>
2020-10-01 16:11:08 +0300

    creates congratulation modal

---
+++ src/components/congratsModal/Congratulations.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import congratsPicture from "../../images/congratsPicture/congratulations.png";
+import style from "./Congratulations.module.css";
+import modalBackDrop from "../modalBackDrop/ModalBackDrop";
+import CustomHabitModal from "../customHabitModal/CustomHabitModal";
+
+const Congratulations = ({ fromCheckList, habitName, close }) => {
+  const [isShowModal, setIsShowModal] = useState(false);
+  const [isShowModalEmpty, setIsShowModalEmpty] = useState(false);
+
+  const showModal = () => {
+    setIsShowModal(true);
+  };
+  const showModalEmpty = () => {
+    setIsShowModalEmpty(true);
+  };
+
+  return (
+    <>
+      <div id="home" className={style.congratulationWrapper}>
+        <div>
+          <h2 className={style.congratulationTitle}>Поздравляем!</h2>
+          <p>Вы успешно освоили привычку и стали на шаг ближе к своей цели.</p>
+        </div>
+        <div>
+          <img
+            src={congratsPicture}
+            alt="congratulations"
+            width="450"
+            height="320"
+          />
+        </div>
+        <div className={style.congratsBtnWrapper}>
+          <button
+            onClick={() => showModal()}
+            className={style.buttonTransparent}
+          >
+            Повторить
+          </button>
+          <button
+            onClick={() => showModalEmpty()}
+            className={style.buttonTransparent}
+          >
+            Добавить новую
+          </button>
+        </div>
+      </div>
+      {isShowModal && (
+        <CustomHabitModal
+          fromCheckList={fromCheckList}
+          habitName={habitName}
+          close={close}
+        />
+      )}
+      {isShowModalEmpty && <CustomHabitModal close={close} />}
+    </>
+  );
+};
+
+export default modalBackDrop(Congratulations);
