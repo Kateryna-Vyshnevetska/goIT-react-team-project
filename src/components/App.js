d78560f0ec673ca848ea99a4e6fe30b84d66de36
Kateryna-Kopchuk <vyshnevetska.k@gmail.com>
2020-10-01 18:38:21 +0300

    fix home

--- src/components/App.js
+++ src/components/App.js
@@ -59,7 +59,6 @@ export function App() {
 
   return (
     <>
-
       {/* <LogInPage /> */}
       {/* <RegisterPage /> */}
 
@@ -72,14 +71,14 @@ export function App() {
       {/* <CustomHabitModal /> */}
       {/* <HabitTemplateModal /> */}
       {/* <HomePage /> */}
-      <CheckListPage />
+      {/* <CheckListPage /> */}
       {/* <Suspense fallback={<Spinner />}> */}
-      <Switch>
-        {/* <PublicRoute exact path="/" /* component={HomePage} */}
-        {/* <PublicRoute path="/logIn" /*  component={LogIn}  */
-        /*/}
+      {/* <Switch> */}
+      {/* <PublicRoute exact path="/" /* component={HomePage} */}
+      {/* <PublicRoute path="/logIn" /*  component={LogIn}  */
+      /*/}
           {/* <PublicRoute path="/signUp" /*  component={SignUp}  */
-        /*/}
+      /*/}
 
 
           <PrivateRoute path="/achievementsPage" component={AchievementsPage} />
@@ -96,28 +95,27 @@ export function App() {
         </Switch>
       </Suspense> */}
 
-          {/* <PrivateRoute */}
-        {/* path="/checkListPage/" /*  component={CheckListPage}  */}
-        {/* /> */}
+      {/* <PrivateRoute */}
+      {/* path="/checkListPage/" /*  component={CheckListPage}  */}
+      {/* /> */}
 
-        {/* <PrivateRoute path="/profilePage" /*  component={ProfilePage}  */
-        /*/}
+      {/* <PrivateRoute path="/profilePage" /*  component={ProfilePage}  */
+      /*/}
           {/* <PrivateRoute */}
-        {/* path="/achievementsPage" /*  component={AchievementsPage}  */}
-        {/* /> */}
-        {/* <PrivateRoute */}
-        {/* path="/notificationsPage" /*  component={NotificationsPage}  */}
-        {/* /> */}
-        {/* <PrivateRoute */}
-        {/* path="/subscriptionsPage" /*  component={SubscriptionsPage}  */}
-        {/* /> */}
-        {/* <PrivateRoute */}
-        {/* path="/chooseAvatarPage" /*  component={ChooseAvatarPage}  */}
-        {/* /> */}
-        {/* <PrivateRoute path="/pageNotFound" /*  component={PageNotFound}  */}
-      </Switch>
+      {/* path="/achievementsPage" /*  component={AchievementsPage}  */}
+      {/* /> */}
+      {/* <PrivateRoute */}
+      {/* path="/notificationsPage" /*  component={NotificationsPage}  */}
+      {/* /> */}
+      {/* <PrivateRoute */}
+      {/* path="/subscriptionsPage" /*  component={SubscriptionsPage}  */}
+      {/* /> */}
+      {/* <PrivateRoute */}
+      {/* path="/chooseAvatarPage" /*  component={ChooseAvatarPage}  */}
+      {/* /> */}
+      {/* <PrivateRoute path="/pageNotFound" /*  component={PageNotFound}  */}
+      {/* </Switch> */}
       {/* </Suspense> */}
-
     </>
   );
 }
