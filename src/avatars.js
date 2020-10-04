a63f6c6c41534d247bc69738408adcd4e4e1b2ca
Peter Ivanovskij <65976810+Ivanovskij-Peter@users.noreply.github.com>
2020-10-04 12:16:23 +0300

    начал делатать логику аватарок

---
+++ src/avatars.js
@@ -0,0 +1,66 @@
+export default [
+  {
+    id: "1",
+    avatar: "avatar1",
+  },
+  {
+    id: "2",
+    avatar: "avatar2",
+  },
+  {
+    id: "3",
+    avatar: "avatar3",
+  },
+  {
+    id: "4",
+    avatar: "avatar4",
+  },
+  {
+    id: "5",
+    avatar: "avatar1",
+  },
+  {
+    id: "6",
+    avatar: "avatar1",
+  },
+  {
+    id: "7",
+    avatar: "avatar1",
+  },
+  {
+    id: "8",
+    avatar: "avatar1",
+  },
+  {
+    id: "9",
+    avatar: "avatar1",
+  },
+  {
+    id: "10",
+    avatar: "avatar1",
+  },
+  {
+    id: "11",
+    avatar: "avatar1",
+  },
+  {
+    id: "12",
+    avatar: "avatar1",
+  },
+  {
+    id: "13",
+    avatar: "avatar1",
+  },
+  {
+    id: "14",
+    avatar: "avatar1",
+  },
+  {
+    id: "15",
+    avatar: "avatar1",
+  },
+  {
+    id: "16",
+    avatar: "avatar1",
+  },
+];
