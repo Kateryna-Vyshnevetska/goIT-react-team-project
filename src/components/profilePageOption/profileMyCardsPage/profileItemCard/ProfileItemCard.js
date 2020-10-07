dda70d19625af3094bfa34504205e756e8d570ad
andrew65215584 <63922036+andrew65215584@users.noreply.github.com>
2020-10-07 19:33:23 +0300

    сделал карусель вторую, поправил стили справа. убрал возможность.

--- src/components/profilePageOption/profileMyCardsPage/profileItemCard/ProfileItemCard.js
+++ src/components/profilePageOption/profileMyCardsPage/profileItemCard/ProfileItemCard.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./profileItemCard.css";
 import { getRandomColor } from "../../../../helpers/getRandomColor";
 
-export const ProfileItemCard = ({ number, timeExpiration }) => {
+export const ProfileItemCard = ({ number, timeExpiration, key }) => {
   return (
     <>
-      <li>
+      <li key={key}>
         <div
           className="ProfileItemCard-cardBlock"
-          style={{ backgroundColor: getRandomColor() }}
+          // style={{ backgroundColor: getRandomColor() }}
         >
           <h4 className="ProfileItemCard-cardName">Моя карта</h4>
           <p className="ProfileItemCard-cardNamber">{number}</p>
